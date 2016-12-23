8c9337c42860bd123c090043b77418d043900490
TedYuen <tedyuen.goo@gmail.com>
2016-12-23 16:52:13 +0800

    Change url to remote server.

--- app/source/js/module/app/urlConstant.js
+++ app/source/js/module/app/urlConstant.js
@@ -3,8 +3,12 @@
  */
 yonglongApp.constant('URL_CONS', {
   // serverUrl:'http://192.168.0.25:8080/admin/api/data',
-  serverUrl:'http://192.168.0.25:8080/admin/api/data',
-  serverFileUrl:'http://192.168.0.25:8080/admin/api/file',
+  // serverUrl:'http://192.168.0.25:8080/admin/api/data',
+  // serverFileUrl:'http://192.168.0.25:8080/admin/api/file',
+  serverUrl:'http://120.26.65.65:8285/adm/api/data',
+  serverFileUrl:' http://120.26.65.65:8285/adm/api/file',
+
+
   companyCreateOrder: 'company_create_order',
   companyOrderList: 'company_list_order',
   companyUserinfo:'company_userinfo',
