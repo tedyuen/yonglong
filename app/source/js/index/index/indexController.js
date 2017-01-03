58697c0de007d57d09c64d59649768ac831d9760
senlin <senlin.bai@wojiawang.com.cn>
2017-01-03 14:42:52 +0800

    Text commit

--- app/source/js/index/index/indexController.js
+++ app/source/js/index/index/indexController.js
@@ -1,3 +1,16 @@
 ylIndex.controller('indexController',['$scope',function ($scope) {
   $scope.testNum = 30001;
+
+
+  $scope.forgetPassCompany = function () {
+    $('#forget-password-company').modal('show');
+    $scope.testNum = 200;
+  }
+
+  $scope.getCodeCompany = function () {
+    console.log('getCodeCompany');
+  }
+
+
+
 }]);
--- app/source/js/index/index/indexcon.js
+++ app/source/js/index/index/indexcon.js
@@ -136,4 +136,14 @@ var ylIndex = angular.module("myApp",[]);
 
 ylIndex.controller('indexController',['$scope',function ($scope) {
   $scope.testNum = 30001;
+
+
+  $scope.forgetPassCompany = function () {
+    $('#forget-password-company').modal('show');
+    $scope.testNum = 200;
+  }
+
+  $scope.getCodeCompany = function () {
+    console.log('getCodeCompany');
+  }
 }]);
