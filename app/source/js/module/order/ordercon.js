a6f9ed220c62e78014c4239564b300ff3fe54541
Baisenlin <senlinaiziji@163.com>
2017-01-19 15:28:12 +0800

    提交订单页增加件数和包装内容

--- app/source/js/module/order/ordercon.js
+++ app/source/js/module/order/ordercon.js
@@ -749,12 +749,15 @@ yonglongApp.service('interfaceService',['httpService','URL_CONS','sessionService
 yonglongApp.constant('rescode', {
   SUCCESS:'0000',
 
-  AGAIN_PHONE:'201002',
-  EMPTY_SMS_CODE:'201004',
+  AGAIN_PHONE:'201004',//手机号已被注册
   ERROR_TOKEN:'9001',
 
   UNKNOW_USER:'201008',//用户不存在
   ERROR_PASSWORD:'201009',//密码不正确
+
+  WRONG_PHONE:'201011',//参数 mobile 格式不正确，mobile手机号格式不正确
+  WRONG_CODE:'201010',//验证码错误
+
 
   HAS_BANK:'301008',//已经绑定过银行卡，换卡请先解绑
 })
